adb03548e44c43da874e6b9b549550aee863e921
Falk33n <tim.falk00@gmail.com>
2025-05-12 15:58:17 +0200

    chore: initial setup of project

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+        env: { publicPrefix: 'PUBLIC_' },
+    }
+};
+
+export default config;
